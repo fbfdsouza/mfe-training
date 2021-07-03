c207c4b57a571d4610ffdf94ed87952d06944aa8
Bruno Filgueiras <fbfdsouza@gmail.com>
2021-07-03 15:37:36 -0300

    initial commit

---
+++ react-parcel/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid } from "uuid";
+import { listenEvent } from "@mc/utils";
+
+const App = () => {
+  const [tasks, updateTasks] = useState([]);
+
+  useEffect(()=>{
+    listenEvent("@mc/react-route/todo-add-todo", event=>{
+      updateTasks(oldTasks => [
+        ...oldTasks,
+        event.detail
+      ])
+    })
+  },[])
+
+  return (
+    <>
+      <h1>@mc/react-parcel</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map(task => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.describe}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
